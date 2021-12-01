58ef6fc38ae562a6bad636b467195603a255a9d4
Steve Madsen <steve@lightyearsoftware.com>
2021-12-01 09:48:07 -0500

    Initial Commit

---
+++ AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Steve Madsen on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
