c984e9faa3dcfc9d0164c9c054d0dc3a1f6586d2
Steve Madsen <steve@lightyearsoftware.com>
2022-12-01 11:41:12 -0500

    2022 day 1 in Elixir

---
+++ elixir/2022/01.exs
@@ -0,0 +1,20 @@
+{:ok, lines} = File.read("1.input")
+calories_per_elf = lines
+|> String.trim()
+|> String.split("\n\n")
+|> Enum.map(fn elf ->
+  elf
+  |> String.split("\n")
+  |> Enum.map(&String.to_integer/1)
+  |> Enum.reduce(0, &+/2)
+end)
+
+max_calories = Enum.max(calories_per_elf)
+IO.puts(max_calories)
+
+top_three =
+  Enum.sort(calories_per_elf, &(&1 > &2))
+  |> Enum.take(3)
+  |> Enum.reduce(0, &+/2)
+
+IO.puts(top_three)
