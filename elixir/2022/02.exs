ef2f97b25e74e35d69baec752a4b88a2fd24e109
Steve Madsen <steve@lightyearsoftware.com>
2022-12-02 10:27:16 -0500

    2022 day 2 Elixir

---
+++ elixir/2022/02.exs
@@ -0,0 +1,26 @@
+defmodule Day2 do
+  @part1_result %{
+    "A X" => 4, "A Y" => 8, "A Z" => 3,
+    "B X" => 1, "B Y" => 5, "B Z" => 9,
+    "C X" => 7, "C Y" => 2, "C Z" => 6
+  }
+
+  def part1(line) do
+    @part1_result[String.trim(line)]
+  end
+
+  @part2_result %{
+    "A X" => 3, "A Y" => 4, "A Z" => 8,
+    "B X" => 1, "B Y" => 5, "B Z" => 9,
+    "C X" => 2, "C Y" => 6, "C Z" => 7
+  }
+
+  def part2(line) do
+    @part2_result[String.trim(line)]
+  end
+end
+
+File.stream!("data/2.input")
+|> Enum.map(&Day2.part2/1)
+|> Enum.reduce(0, &+/2)
+|> IO.puts()
