e778cbac2446fc2ad0785818017f5d7a5ae53208
Steve Madsen <steve@lightyearsoftware.com>
2022-12-04 10:11:45 -0500

    2022 day 4 in Elixir

---
+++ elixir/2022/04.exs
@@ -0,0 +1,27 @@
+defmodule Day4 do
+  def parse_ranges(line) do
+    [first, second] = line
+    |> String.split(",")
+    |> Enum.map(fn range ->
+      [low, high] = String.split(range, "-")
+      Range.new(String.to_integer(low), String.to_integer(high))
+    end)
+    {MapSet.new(first), MapSet.new(second)}
+  end
+end
+
+File.stream!("data/4.input")
+|> Enum.map(&String.trim/1)
+|> Enum.map(&Day4.parse_ranges/1)
+|> Enum.count(fn {first, second} ->
+  MapSet.subset?(first, second) || MapSet.subset?(second, first)
+end)
+|> IO.puts
+
+File.stream!("data/4.input")
+|> Enum.map(&String.trim/1)
+|> Enum.map(&Day4.parse_ranges/1)
+|> Enum.count(fn {first, second} ->
+  !Enum.empty?(MapSet.intersection(first, second))
+end)
+|> IO.puts
