b912e752382dd8b0214b9d7f9156bd1bfb38f3c5
Steve Madsen <steve@lightyearsoftware.com>
2022-12-04 09:47:45 -0500

    2022 day 3 in Elixir

---
+++ elixir/2022/03.exs
@@ -0,0 +1,33 @@
+defmodule Day3 do
+  def value(c) when c > ?a, do: c - ?a + 1
+  def value(c), do: c - ?A + 27
+end
+
+File.stream!("data/3.input")
+|> Enum.map(&String.trim/1)
+|> Enum.map(fn line ->
+  {first, second} = String.split_at(line, div(String.length(line), 2))
+  first = MapSet.new(String.to_charlist(first))
+  second = MapSet.new(String.to_charlist(second))
+  MapSet.intersection(first, second)
+  |> MapSet.to_list
+  |> List.first
+  |> Day3.value
+end)
+|> Enum.reduce(0, &+/2)
+|> IO.puts
+
+File.stream!("data/3.input")
+|> Enum.map(&String.trim/1)
+|> Enum.chunk_every(3)
+|> Enum.map(fn chunk ->
+  chunk
+  |> Enum.map(&String.to_charlist/1)
+  |> Enum.map(&MapSet.new/1)
+  |> Enum.reduce(&MapSet.intersection/2)
+  |> MapSet.to_list
+  |> List.first
+  |> Day3.value
+end)
+|> Enum.reduce(0, &+/2)
+|> IO.puts
