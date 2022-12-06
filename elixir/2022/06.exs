adbfe22d588d2243e7441058796c5052966046cf
Steve Madsen <steve@lightyearsoftware.com>
2022-12-06 12:09:22 -0500

    2022 day 6 in Elixir

---
+++ elixir/2022/06.exs
@@ -0,0 +1,27 @@
+defmodule Day6 do
+  def part1(string), do: find_unique_suffix(string, 3, 4)
+
+  def part2(string), do: find_unique_suffix(string, 13, 14)
+
+  defp find_unique_suffix(string, index, count) do
+    unique = string
+    |> String.slice(index, count)
+    |> String.to_charlist
+    |> MapSet.new
+    if Enum.count(unique) == count do
+      index + count
+    else
+      find_unique_suffix(string, index + 1, count)
+    end
+  end
+end
+
+input = File.read!("data/6.input")
+
+input
+|> Day6.part1
+|> IO.puts
+
+input
+|> Day6.part2
+|> IO.puts
