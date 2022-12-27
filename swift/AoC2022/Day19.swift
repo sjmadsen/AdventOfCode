fffbd2f25d10e45cfbd641f4872a49b60eaa4f69
Steve Madsen <steve@lightyearsoftware.com>
2022-12-27 11:01:54 -0500

    2022 day 19 part 2 in Swift

--- swift/AoC2022/Day19.swift
+++ swift/AoC2022/Day19.swift
@@ -18,7 +18,7 @@ Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsid
 
     typealias Blueprint = (oreRobot: Int, clayRobot: Int, obsidianRobot: (ore: Int, clay: Int), geodeRobot: (ore: Int, obsidian: Int))
 
-    struct State {
+    struct State: Hashable {
         var ore = 0
         var clay = 0
         var obsidian = 0
@@ -31,8 +31,8 @@ Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsid
 
     override init() {
         input =
-//        try! String(contentsOf: Bundle.main.url(forResource: "day19", withExtension: "input")!)
-        sample
+        try! String(contentsOf: Bundle.main.url(forResource: "day19", withExtension: "input")!)
+//        sample
             .trimmed
             .split(separator: "\n")
             .map { Day19.parse(blueprint: String($0)) }
@@ -45,97 +45,95 @@ Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsid
         return (oreRobot: Int(match.1)!, clayRobot: Int(match.2)!, obsidianRobot: (ore: Int(match.3)!, clay: Int(match.4)!), geodeRobot: (ore: Int(match.5)!, obsidian: Int(match.6)!))
     }
 
-    func simulate(_ blueprint: Blueprint, state: State, minute: Int = 1, limit: Int, maximum: inout Int) -> State {
-        guard minute <= limit else { return state }
+    func simulate(_ blueprint: Blueprint, limit: Int) -> State {
+        var queue = Set([State()])
+        for minute in 0..<limit {
+            let remaining = limit - minute
+            let maximum = queue
+                .map {
+                    $0.geodeRobots * remaining + (remaining * (remaining - 1) / 2) + $0.geodes
+                }
+                .max()!
+            print("Minute \(minute + 1), \(queue.count) states to consider, best possible from here is \(maximum)")
+            var nextQueue = Set<State>()
+            while let state = queue.popFirst() {
+                let bestPossible = state.geodeRobots * remaining + (remaining * (remaining - 1) / 2) + state.geodes
+                if Double(bestPossible) < Double(maximum) * 0.8 {
+                    continue
+                }
+                if state.ore >= blueprint.geodeRobot.ore, state.obsidian >= blueprint.geodeRobot.obsidian {
+                    var state = state
+                    state.ore -= blueprint.geodeRobot.ore
+                    state.obsidian -= blueprint.geodeRobot.obsidian
+                    state.ore += state.oreRobots
+                    state.clay += state.clayRobots
+                    state.obsidian += state.obsidianRobots
+                    state.geodes += state.geodeRobots
+                    state.geodeRobots += 1
+                    nextQueue.insert(state)
+                }
+                if state.ore >= blueprint.obsidianRobot.ore, state.clay >= blueprint.obsidianRobot.clay {
+                    var state = state
+                    state.ore -= blueprint.obsidianRobot.ore
+                    state.clay -= blueprint.obsidianRobot.clay
+                    state.ore += state.oreRobots
+                    state.clay += state.clayRobots
+                    state.obsidian += state.obsidianRobots
+                    state.geodes += state.geodeRobots
+                    state.obsidianRobots += 1
+                    nextQueue.insert(state)
+                }
+                if state.ore >= blueprint.clayRobot, state.clayRobots < blueprint.obsidianRobot.clay {
+                    var state = state
+                    state.ore -= blueprint.clayRobot
+                    state.ore += state.oreRobots
+                    state.clay += state.clayRobots
+                    state.obsidian += state.obsidianRobots
+                    state.geodes += state.geodeRobots
+                    state.clayRobots += 1
+                    nextQueue.insert(state)
+                }
+                if state.ore >= blueprint.oreRobot, state.oreRobots < [blueprint.oreRobot, blueprint.clayRobot, blueprint.obsidianRobot.ore, blueprint.geodeRobot.ore].max()! {
+                    var state = state
+                    state.ore -= blueprint.oreRobot
+                    state.ore += state.oreRobots
+                    state.clay += state.clayRobots
+                    state.obsidian += state.obsidianRobots
+                    state.geodes += state.geodeRobots
+                    state.oreRobots += 1
+                    nextQueue.insert(state)
+                }
 
-        let timeRemaining = limit - minute
-        if state.geodeRobots * (timeRemaining + 1) + max(0, (timeRemaining * (timeRemaining - 1) / 2)) + state.geodes < maximum {
-//            return state
-        }
-
-        var choices = [State]()
-        if state.ore >= blueprint.geodeRobot.ore, state.obsidian >= blueprint.geodeRobot.obsidian {
-            var state = state
-            state.ore -= blueprint.geodeRobot.ore
-            state.obsidian -= blueprint.geodeRobot.obsidian
-            state.ore += state.oreRobots
-            state.clay += state.clayRobots
-            state.obsidian += state.obsidianRobots
-            state.geodes += state.geodeRobots
-            state.geodeRobots += 1
-            let result = simulate(blueprint, state: state, minute: minute + 1, limit: limit, maximum: &maximum)
-            if result.geodes > maximum {
-                print("new best \(result)")
-                maximum = result.geodes
+                var state = state
+                state.ore += state.oreRobots
+                state.clay += state.clayRobots
+                state.obsidian += state.obsidianRobots
+                state.geodes += state.geodeRobots
+                nextQueue.insert(state)
             }
-            return result
-        }
-        if state.ore >= blueprint.obsidianRobot.ore, state.clay >= blueprint.obsidianRobot.clay {
-            var state = state
-            state.ore -= blueprint.obsidianRobot.ore
-            state.clay -= blueprint.obsidianRobot.clay
-            state.ore += state.oreRobots
-            state.clay += state.clayRobots
-            state.obsidian += state.obsidianRobots
-            state.geodes += state.geodeRobots
-            state.obsidianRobots += 1
-            choices.append(simulate(blueprint, state: state, minute: minute + 1, limit: limit, maximum: &maximum))
-        }
-        if state.ore >= blueprint.clayRobot, state.clayRobots < blueprint.obsidianRobot.clay {
-            var state = state
-            state.ore -= blueprint.clayRobot
-            state.ore += state.oreRobots
-            state.clay += state.clayRobots
-            state.obsidian += state.obsidianRobots
-            state.geodes += state.geodeRobots
-            state.clayRobots += 1
-            choices.append(simulate(blueprint, state: state, minute: minute + 1, limit: limit, maximum: &maximum))
-        }
-        if state.ore >= blueprint.oreRobot, state.oreRobots < [blueprint.oreRobot, blueprint.clayRobot, blueprint.obsidianRobot.ore, blueprint.geodeRobot.ore].max()! {
-            var state = state
-            state.ore -= blueprint.oreRobot
-            state.ore += state.oreRobots
-            state.clay += state.clayRobots
-            state.obsidian += state.obsidianRobots
-            state.geodes += state.geodeRobots
-            state.oreRobots += 1
-            choices.append(simulate(blueprint, state: state, minute: minute + 1, limit: limit, maximum: &maximum))
+            queue = nextQueue
         }
 
-        var state = state
-        state.ore += state.oreRobots
-        state.clay += state.clayRobots
-        state.obsidian += state.obsidianRobots
-        state.geodes += state.geodeRobots
-        choices.append(simulate(blueprint, state: state, minute: minute + 1, limit: limit, maximum: &maximum))
-
-        let bestChoice = choices.max { $0.geodes < $1.geodes }!
-        if maximum < bestChoice.geodes {
-            maximum = bestChoice.geodes
-            print("new best \(maximum)")
-        }
-        return bestChoice
+        return queue.max { $0.geodes < $1.geodes }!
     }
 
     override func compute() async {
-//        let part1_result = input
-//            .enumerated()
-//            .map {
-//                var maximum = 0
-//                let result = simulate($1, state: State(), limit: 24, maximum: &maximum)
-//                print($0 + 1, result, ($0 + 1) * result.geodes)
-//                return ($0 + 1) * result.geodes
-//            }
-//            .reduce(0, +)
-//        await MainActor.run {
-//            part1 = String(part1_result)
-//        }
+        let part1_result = input
+            .enumerated()
+            .map {
+                let result = simulate($1, limit: 24)
+                print($0 + 1, result, ($0 + 1) * result.geodes)
+                return ($0 + 1) * result.geodes
+            }
+            .reduce(0, +)
+        await MainActor.run {
+            part1 = String(part1_result)
+        }
 
         let part2_result = input.prefix(3)
             .enumerated()
             .map {
-                var maximum = 0
-                let result = simulate($1, state: State(), limit: 32, maximum: &maximum)
+                let result = simulate($1, limit: 32)
                 print($0, result)
                 return result.geodes
             }
